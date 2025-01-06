6a4a63d50584ac8ac830982400acd64f84d6b1e4
Behailu Sileshi <behailusileshi000@gmail.com>
2025-01-06 19:18:09 +0300

    Create a modelviewset for post and register it to router.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from email.mime import base
+from rest_framework_nested import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register('posts', views.PostViewSet, basename='posts')
+
+urlpatterns = router.urls
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated
+from .serializers import PostSerializer
+from .models import Author, Post
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    
+    def get_queryset(self):
+        author = Author.objects.filter(user_id=self.request.user.id).first()
+        if self.request.method == 'GET':
+            return Post.objects.all()
+        return Post.objects.filter(owner=author)
+    
+    
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
+    
+    
