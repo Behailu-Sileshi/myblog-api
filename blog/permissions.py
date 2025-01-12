38029ae6057b6592208e7ba9abc193c539f124af
Behailu Sileshi <behailusileshi000@gmail.com>
2025-01-12 10:19:01 +0300

    Define a custom permission class for postimage/postvideo view.

--- blog/permissions.py
+++ blog/permissions.py
@@ -1,12 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import permissions
 
-
-class IsTheAuthor(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.owner.user_id == request.user.id
-
+from .models import Post, Author
 
 
 class DenyUpdateExceptMe(permissions.BasePermission):
@@ -14,13 +9,35 @@ class DenyUpdateExceptMe(permissions.BasePermission):
         if view.action == 'update' and view.action != 'me':
             return False
         return True
+ 
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.author == obj.owner
 
 
-class DenyPostDeleteExceptFollowUnfollow(permissions.BasePermission):
+class IsPostOwnerOrReadOnly(permissions.BasePermission):
+    def is_owner(self, request, view):
+        post_pk = view.kwargs.get('post_pk')
+        post = get_object_or_404(Post, pk=post_pk)
+        author = Author.objects.filter(user=request.user.id).first()
+        return post.owner == author
+        
+
     def has_permission(self, request, view):
-        if request.method in ['POST', 'DELETE']:
-            if view.action not in ['follow', 'unfollow']:
-                return False
-        return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return self.is_owner(request, view)
 
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return self.is_owner(request, view)
+
+    
+    
+
+        
 
